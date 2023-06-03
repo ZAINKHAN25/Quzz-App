9f4bb960ffa7a0f9b440278cefbe73a92d2f6417
ZAINKHAN25 <zainarfeen87@gmail.com>
2023-06-04 00:23:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function htmlpagerender(){
+  location.href = "./htmlquiz.html";
+} 
+function csspagerender(){
+  location.href = "./cssquiz.html";
+} 
+function javascirptpagerender(){
+  location.href = "./javascriptquiz.html";
+} 
